0ea8e6958183b8b69dfed335c25631b949cf8387
EragonJ <eragonj@eragonj.me>
2013-05-22 22:21:41 +0800

    make anchor block & add delay to hide logo-intro

--- public/js/index.js
+++ public/js/index.js
@@ -7,8 +7,16 @@ $(document).ready(function() {
     });
 
     // animate
+    var delay = 1800;
+
     $(".logo-up").addClass("animated fadeOutUpBig");
     $(".logo-down").addClass("animated fadeOutDownBig");
+
+    setTimeout(function() {
+        console.log('x');
+        $(".logo-intro").addClass("hide");
+    }, delay + 1000);
+
 });
 
 function resizeAll() {
@@ -31,3 +39,4 @@ function setSlidesSize(h) {
 function showMemberInfo( e ) {
     console.log( e );
 }
+
