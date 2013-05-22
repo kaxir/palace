b5bd57238802e26dbac66643742b6cef53d6acd4
EragonJ <eragonj@eragonj.me>
2013-05-22 17:10:19 +0800

    update compass config

--- config.rb
+++ config.rb
@@ -2,9 +2,9 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "public/css"
-sass_dir = "public/scss"
-images_dir = "public/img"
+css_dir = "./public/css"
+sass_dir = "./public/scss"
+images_dir = "./public/img"
 javascripts_dir = "public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
